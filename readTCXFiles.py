5111870d0cb1dab6a17e86164d4ef6504c5b9db3
tdargent <thomas.dargent@univ-lille.fr>
2017-03-27 16:25:22 +0200

    code "commented" and "cleaned"

--- readTCXFiles.py
+++ readTCXFiles.py
@@ -2,27 +2,18 @@
 
 import os,tcxparser
 
-cd = os.path.dirname(os.path.abspath(__file__)) #retourne le dossier dans lequel est enregistre ce code python"
-print(cd) 
-
-path=os.getcwd() #retourne le dossier dans lequel est enregistre ce code python"
-print(path)
-
 file_list=os.listdir(os.getcwd())
 
 f=open("result.txt","a")
 f.write("date (ISO UTC Format)"+'\t'+"Distance [m]"+'\t'+"Duration [s]"+'\n')
 for a in file_list:
     if a == "result.txt": 
-        print()
+        print()     #Quick and dirty...
     elif a== "readTCXFiles.py":
-        print()
+        print()     #Quick and dirty...
     else:    
-        tcx=tcxparser.TCXParser(cd+'/'+a)
-        if tcx.activity_type == 'biking'  :  
-            #print(tcx.completed_at,tcx.distance,tcx.duration)
-            #f=open("result.txt","a")
+        tcx=tcxparser.TCXParser(cd+'/'+a) #see https://github.com/vkurup/python-tcxparser/ for details
+        if tcx.activity_type == 'biking'  :  #To select only my biking session (could be change to 'hiking', 'running' etc.)
             f.write(str(tcx.completed_at)+'\t'+str(tcx.distance)+'\t'+str(tcx.duration)+'\n')
 
-f.close			
-print("Done.")
+f.close
