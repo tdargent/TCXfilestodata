b8db018f1e76b4def9a38b26c0f1c818c43965a3
tdargent <thomas.dargent@univ-lille.fr>
2017-03-28 11:22:18 +0200

    code-fixing

--- readTCXFiles.py
+++ readTCXFiles.py
@@ -14,6 +14,7 @@ for a in file_list:
     else:    
         tcx=tcxparser.TCXParser(cd+'/'+a) #see https://github.com/vkurup/python-tcxparser/ for details
         if tcx.activity_type == 'biking'  :  #To select only my biking session (could be change to 'hiking', 'running' etc.)
+            f=open("result.txt","a")
             f.write(str(tcx.completed_at)+'\t'+str(tcx.distance)+'\t'+str(tcx.duration)+'\n')
 
 f.close
