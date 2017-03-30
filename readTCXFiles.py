3ed1eb221cebeffc566ad7fcb70178f35fb67000
tdargent <thomas.dargent@univ-lille.fr>
2017-03-30 15:43:39 +0200

    simplify(?) code using array
    each cells is a tuple (?)  of date, distance and duration

--- readTCXFiles.py
+++ readTCXFiles.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os,tcxparser
+import numpy as np
 
 cd = os.path.dirname(os.path.abspath(__file__)) #use to browse tcx files at the correct location
 file_list=os.listdir(os.getcwd())
+b=[]
 
-f=open("result.txt","a")
-f.write("date (ISO UTC Format)"+'\t'+"Distance [m]"+'\t'+"Duration [s]"+'\n')
 for a in file_list:
     if a == "result.txt": 
         print()     #Quick and dirty...
@@ -15,5 +15,6 @@ for a in file_list:
     else:    
         tcx=tcxparser.TCXParser(cd+'/'+a) #see https://github.com/vkurup/python-tcxparser/ for details
         if tcx.activity_type == 'biking'  :  #To select only my biking session (could be change to 'hiking', 'running' etc.)
-            f.write(str(tcx.completed_at)+'\t'+str(tcx.distance)+'\t'+str(tcx.duration)+'\n')
-f.close
+            b.append((tcx.completed_at,tcx.distance,tcx.duration))
+         
+
